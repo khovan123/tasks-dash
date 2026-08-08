18deba44702dc0d29b026a75bae76e2dbfe7dc6f
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-08 14:45:15 +0700

    refactor(pages): import automation organisms directly

--- apps/web/src/app/projects/[projectKey]/automations/page.tsx
+++ apps/web/src/app/projects/[projectKey]/automations/page.tsx
@@ -1,9 +1,9 @@
 import { MEMBER_ROLES } from "@tasks-dash/contracts";
-import { AutomationCreateForm } from "@/components/automation-create-form";
+import { AutomationCreateForm } from "@/components/organisms/automation-create-form";
 import {
   AutomationRuleManager,
   type AutomationRule,
-} from "@/components/automation-rule-manager";
+} from "@/components/organisms/automation-rule-manager";
 import { SectionHeading } from "@/components/molecules/section-heading";
 import { AppPage } from "@/components/templates/app-page";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
